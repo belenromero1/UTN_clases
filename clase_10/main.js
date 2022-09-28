01fe6c142d26d6005fddd25aaddc8368ccd33671
belenr1 <maria.bemero01@gmail.com>
2022-09-28 13:17:33 -0300

    intro JS

---
+++ clase_10/main.js
@@ -0,0 +1,39 @@
+//Concatenar ejemplo
+alert ("hola"+ "mundo" + (9 + 10))
+
+//var ES5/ en ES6=js moderno  ya no se usa var
+let nombre ="Belen";
+let apellido ="Romero";
+let edad =21;
+let mascotas = true;
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(mascotas)
+ // TIPOS DE DATOS
+ //boolean : true :1 /false :0
+ //Number : enteros, decimales,negativos
+let numeroA = 12;
+console.log(numeroA)
+let numeroB = 3;
+console.log(numeroB)
+
+let resultadoSuma = numeroA + numeroB;
+console.log(resultadoSuma)
+
+let resultadoMul = numeroA * numeroB;
+console.log(resultadoMul);
+
+//variable
+let numeroFav = 36
+
+//constante
+const pi = 3-14
+
+let score = 2
+console.log(score)
+// aumentamos el score en 1 tres maneras diferentes
+score = score + 1
+score +=5 // score = score + 5
+score++ // es lo mismo que score + 1
